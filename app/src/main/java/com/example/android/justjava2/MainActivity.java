71276d97f282c32837c39c852a67fcd8d370470a
RayceeM <mwatelaraycee33@gmail.com>
2018-03-19 17:02:02 +0300

    added images,intent and extra functionality

--- app/src/main/java/com/example/android/justjava2/MainActivity.java
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -9,12 +9,14 @@
 package com.example.android.justjava2;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -33,11 +35,29 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price=quantity*5;
-        String priceMessage="Total $" +(price);
-        priceMessage= priceMessage + "\n thank you!";
-        displayMessage(priceMessage);
+
+        CheckBox whippedCreamCheckBox=(CheckBox) findViewById(R.id.whipped_cream_checkbox);
+       boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox=(CheckBox)findViewById(R.id.chocolate_checkbox);
+       boolean hasChocolate=chocolateCheckBox.isChecked();
+
+
+        EditText txt = (EditText)findViewById(R.id.name_field);
+        String name = txt.getText().toString();
+
+        int price=calculatePrice(hasWhippedCream,hasChocolate);
+        String priceMessage=createOrderSummary( name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "just java order for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
+
     public void increment(View view) {
         quantity=quantity+1;
         display(quantity);
@@ -45,6 +65,18 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         quantity=quantity-1;
         display(quantity);
+
+    }
+
+    private String createOrderSummary(String name,int price,boolean addWhippedCream,boolean addChocolate){
+        String priceMessage="Name:" + name;
+        priceMessage=priceMessage + "\n add whipped cream?" + addWhippedCream;
+        priceMessage=priceMessage + "\n add chocolate?" + addChocolate;
+        priceMessage=priceMessage + "\n Quantity: " + quantity;
+        priceMessage=priceMessage + "\n Total: $" +(price);
+        priceMessage= priceMessage + "\n thank you!";
+
+        return priceMessage;
 
     }
     /**
@@ -55,17 +87,20 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-    /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    /**
+     * Calculates the price of the order.
+          */
+    private int calculatePrice(boolean addWhippedCream,boolean addChocolate) {
+        int basePrice=5;
+
+        if(addWhippedCream){
+            basePrice = basePrice+1;
+        }
+        if(addChocolate){
+            basePrice = basePrice+2;
+        }
+        return quantity * basePrice;
     }
 }
